4b2799b815f2186f39b6d22885c8b17468ddf77a
Лев Романович Панас <jieba2005@list.ru>
2025-04-15 23:40:03 +0300

    first objectives

---
+++ BiblioDesk.py
@@ -0,0 +1,19 @@
+from datetime import datetime  # хочу, чтобы была история добавление книги
+
+class Library:
+    
+    def __init__(self):
+        self.books = []
+    
+    # проверка, есть ли книга в базе
+    def сheck_book():
+        pass
+
+    # добавление книги в базу
+    def add_book():
+        pass
+
+# Нужно название и билет, чтобы была какая то история
+library = Library()
+book = input('Введине название книги: ')
+ticket = input('Введите читательский билет: ')
