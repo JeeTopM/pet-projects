6791517e5bebb8764c2b0babc1a060062d94a7c8
Лев Романович Панас <jieba2005@list.ru>
2025-04-16 12:49:01 +0300

    BiblioDesk v.1

--- BiblioDesk.py
+++ BiblioDesk.py
@@ -1,19 +1,32 @@
 from datetime import datetime  # хочу, чтобы была история добавление книги
 
 class Library:
-    
     def __init__(self):
-        self.books = []
+        self.books_titles = []
     
-    # проверка, есть ли книга в базе
-    def сheck_book():
-        pass
+    def сheck_book(self, book_title):
+        return any(book["title"] == book_title for book in self.books_titles)
 
-    # добавление книги в базу
-    def add_book():
-        pass
+    def add_book(self, book_title, ticket):
+        if not self.сheck_book(book_title):
+            book = {
+                'title': book_title, 
+                'ticket': ticket, 
+                'dt': datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+                }
+            self.books_titles.append(book)
+            print('Книга внесена в базу')
+        else:
+            print('Книга уже в базе')
 
-# Нужно название и билет, чтобы была какая то история
-library = Library()
-book = input('Введине название книги: ')
-ticket = input('Введите читательский билет: ')
+    def all_books(self):
+        return self.books_titles
+    
+if __name__ == "__main__":
+    library = Library()
+    book_title = input('Введине название книги: ')
+    ticket = input('Введите читательский билет: ')
+    library.add_book(book_title, ticket)
+
+    for book in library.all_books():
+        print(book)
